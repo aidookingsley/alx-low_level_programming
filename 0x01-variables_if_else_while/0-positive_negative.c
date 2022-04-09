dad1ca9258094dda19ea696d4968f637be28e561
aidookingsley <kaidoog36@gmail.com>
2022-04-09 06:11:36 -0700

    adding 0-positive_or_negative.c file

--- 0x01-variables_if_else_while/0-positive_negative.c
+++ 0x01-variables_if_else_while/0-positive_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints if the number is positive, zero or negative
+ * main - Prints if number is positive, zero or negative
  *
- * Return: Always (Success)
+ * Return Always (Success)
  */
 int main(void)
 {
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if (n > 0 )
 	{
 		printf("%d is positive\n", n);
 	}
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	else 
+	else
 	{
 		printf("%d is negative\n", n);
 	}
