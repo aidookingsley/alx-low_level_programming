8ef3f11f55b6e1acb50a2b175b8ca4e06de9481f
aidookingsley <kaidoog36@gmail.com>
2022-04-09 16:21:25 -0700

    adding 100-print_comb3.c file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,7 +18,7 @@ int main(void)
 				putchar(c);
 				putchar(i);
 
-				if ( c != '8' || (c == '8' && i != '9'))
+				if (c != '8' || (c == '8' && i != '9'))
 				{
 					putchar(',');
 					putchar(' ');
