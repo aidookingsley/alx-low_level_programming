0ff4e49b2475ff97d15d57b9bb45034cae37c267
aidookingsley <kaidoog36@gmail.com>
2022-04-12 17:40:16 -0700

    9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,10 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
+
 	for (a = 0; a <= 9; a++)
 	{
-		for (b =0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
 			if ((c / 10) == 0)
