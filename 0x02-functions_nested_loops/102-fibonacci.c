2974b27578ab676ff2ba242295e9c6818673a765
aidookingsley <kaidoog36@gmail.com>
2022-04-13 15:48:30 -0700

    102-fibonacci.c file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 fibonacci
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int i, x = 1, y = 2, sum = 0;
+	
+	for (i = 0; i < 49; i++)
+	{
+		printf("%ld, ", x);
+		sum = x + y;
+		x = y;
+		y = sum;
+
+		if (i == 48)
+			printf("%d\n", x);
+	}
+	return (0);
+}
