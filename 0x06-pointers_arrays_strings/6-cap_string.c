508688cf1db205deb127eae652d5ba63b53f2559
aidookingsley <kaidoog36@gmail.com>
2022-04-23 13:08:59 -0700

    Write a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,31 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string.
- * @s: string to convert character from.
- * Return: Converted string.
+ * cap_string - capitalizes words in a string
+ * @n: string to convert cahracter from
+ * Return: Converted string
  */
 
+char *cap_string(char *s)
+{
+	int count = 0, i;
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-char *cap_string(char *n)
-{
-int count = 0, i;
-int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-
-if (*(s + count) >= 97 && *(s + count) <= 122)
-*(s + count) = *(s + count) - 32;
-count++;
-while (*(s + count) != '\0')
-{
-for (i = 0; i < 13; i++)
-{
-if (*(s + count) == separators[i])
-{
-if ((*(s + count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-*(s + (count + 1)) = *(s + (count + 1)) - 32;
-break;
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == separators[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
+	}
+return (s);	
 }	
-}
-count++;
-}
-return (s);
-}
