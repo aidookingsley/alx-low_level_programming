dd52fe3ab13f3fae3d9cf6cc1c7f15dcb97b81e3
aidookingsley <kaidoog36@gmail.com>
2022-04-23 12:29:37 -0700

    Write a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,37 +7,27 @@
  * Return: Converted string.
  */
 
+
 char *cap_string(char *n)
 {
-	int i;
+	int count = 0, i;
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	i = 0;
-	if (n[0] >= 'a' && n[0] <= 'z')
-	{
-		n[0] n[0] - 32;
-	}
-	for (i = 0; n[i] != '\0'; i++)
-	{
-		switch (n[i])
-		{
-			case ',':
-			case ';':
-			case '.':
-			case '!':
-			case '?':
-			case '"':
-			case '(':
-			case ')':
-			case '{':
-			case '}':
-			case ' ':
-			case '\n':
-			case '\t':
-				if (n[i + 1] > 96 && n[i + 1] < 123)
-				{
-					n[i + 1] = n[i + 1] - 32;
-				}	
-		}	
-	}
-	return (n);	
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 13; i++)
+{
+if (*(s + count) == separators[i])
+{
+if ((*(s + count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
+}	
+}
+count++;
+}
+return (s);
 }
