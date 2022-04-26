acc58778ba94a74d14086d8e545adc8e3cef9604
aidookingsley <kaidoog36@gmail.com>
2022-04-26 13:57:54 -0700

    input

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _memcpy - copy number bytes form address "from" to address "to"
+ * @dest: content is to be copied
+ * @src: Source of data to be copied
+ * @n: bytes of the memory
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n) /*Declaring WHILE*/
+	{
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
+		i++;
+
+	} /*END WHILE*/
+
+
+	return (dest);
+}
+
+
