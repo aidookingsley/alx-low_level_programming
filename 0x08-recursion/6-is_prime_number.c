eb1abd29403a6f56e6b7fb2983cd0accf7b3b24a
aidookingsley <kaidoog36@gmail.com>
2022-04-28 16:08:03 -0700

    This function returns 1 if the input integer is a prime number, otherwise returns 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -30,7 +30,7 @@ int evaluate_num(int num, int iterator)
 }
 
 /**
- * 6-is_prime_number - evaluate prime or not
+ * is_prime_number - evaluate prime or not
  * @num: number
  * Return: return 1 prime - return 0 otherwise
  */
