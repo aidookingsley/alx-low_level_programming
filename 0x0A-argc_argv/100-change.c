481d32b7557a6557179c2f5909d22440741eac18
aidookingsley <kaidoog36@gmail.com>
2022-05-04 19:07:41 -0700

    This program prints the min number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	int position, total, change, aux;
 	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
 
-	position = total = change = aux= 0;
+	position = total = change = aux = 0;
 
 	if (argc != 2)
 	{
