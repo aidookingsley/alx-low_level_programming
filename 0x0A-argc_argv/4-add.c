990b2cf2d76a4c67ff0bb4249f0e461a927d5c08
aidookingsley <kaidoog36@gmail.com>
2022-05-04 18:15:25 -0700

    This program adds two positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,13 +4,14 @@
 #include <string.h>
 
 /**
- * check_num - check string if there are digit
+ * check_num: - check string if there are digit
  * @str: array str
  *
  * Return; Always 0 (Success)
  */
 
 int check_num(char *str)
+
 {
 	/*Declaring variables*/
 	unsigned int count;
