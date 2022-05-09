ab20a471848d26724266fce1f7aec3018ca3efc1
aidookingsley <kaidoog36@gmail.com>
2022-05-09 16:00:56 -0700

    This function allocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,6 +16,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		clone = ptr;
 	else
 	{ return (malloc(new_size)); }
+	if (new_size == old_size)
+		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{ free(ptr);
 		return (0); }
