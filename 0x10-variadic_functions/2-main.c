064c2bf2cc382758cb5264c1f2768a9a17fac91f
aidookingsley <kaidoog36@gmail.com>
2022-05-16 20:02:57 -0700

    setup

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code for print strings.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
+}
