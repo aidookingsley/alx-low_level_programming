d61dea95c0705ba2878017c098cc6f80b6952a9a
aidookingsley <kaidoog36@gmail.com>
2022-05-26 15:02:41 -0700

    my commit

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,15 +9,15 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new;
+	listint_t *new;
 
-    new = malloc(sizeof(listint_t));
-    if (!new)
-        return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 
-    new->n = n;
-    new->next = *head;
-    *head = new;
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
-    return (new);
+	return (new);
 }
