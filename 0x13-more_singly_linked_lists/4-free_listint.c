acd4d37dfb8e036c9c3cd6161e3435dfcbe2e537
aidookingsley <kaidoog36@gmail.com>
2022-05-26 15:13:04 -0700

    my commit

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-    listint_t *temp;
+	listint_t *temp;
 
-    while (head)
-    {
-        temp = head->next;
-        free(head);
-        head = temp;
-    }
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
