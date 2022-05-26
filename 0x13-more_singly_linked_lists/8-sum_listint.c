bb1479a089dcc5a879316cff4b298f1bb3672a64
aidookingsley <kaidoog36@gmail.com>
2022-05-26 15:43:37 -0700

    my commit

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,14 +8,14 @@
  */
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
-    listint_t *temp = head;
+	int sum = 0;
+	listint_t *temp = head;
 
-    while (temp)
-    {
-        sum += temp->n;
-        temp = temp->next;
-    }
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
 
-    return (sum);
+	return (sum);
 }
