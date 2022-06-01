6ab1dc6eb9b0bc1c45fc64b276756edf8f7608a8
aidookingsley <kaidoog36@gmail.com>
2022-05-31 18:29:30 -0700

    my commit

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - convert binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
+ */
+unsigned int binary_to_unit(const char *b)
+{
+	int len = 0, j;
+	unsigned int number = 0, non_binary = 0;
+
+	if (b == NULL)
+		return (non_binary);
+
+	while (b[len] != '\0')
+		len++;
+	len -= 1;
+
+	j = 0;
+	while (b[j])
+	{
+		if ((b[j] != '0') && (b[j] != '1'))
+			return (non_binary);
+
+		if (b[j] == '1')
+			number += (1 * (1 << len));
+		j++;
+		len--;
+	}
+
+	return (number);
+}
